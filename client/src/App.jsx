4a35f3af40b56ec2ef25e33ec9a68ce286a7c073
Carlos Quiroz <cquiroz7869@gmail.com>
2024-11-21 00:38:00 -0800

    login page & url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 
-import ExamplePage from "./pages/ExamplePage";
+import Login from "./pages/Login";
 
 // all routes used in web app lie here
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={ <ExamplePage /> } />
+        <Route path="/login" element={ <Login /> } />
       </Routes>
     </>
   );
