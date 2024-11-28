fffcf5471ee0fa871a6b6ad4135c7a24e5e2157e
Carlos Quiroz <cquiroz7869@gmail.com>
2024-11-27 20:10:35 -0800

    integrated jwt

--- server/backend/src/main/java/com/cs157a/backend/controller/AuthController.java
+++ server/backend/src/main/java/com/cs157a/backend/controller/AuthController.java
@@ -5,8 +5,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.cs157a.backend.dal.AdminDAO;
 import com.cs157a.backend.model.Admin;
+import com.cs157a.backend.util.JwtUtil;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +20,9 @@ public class AuthController {
 
     @Autowired
     private AdminDAO adminDAO;
+    
+    @Autowired
+    private JwtUtil jwtUtil;
 
     // Post request
     @PostMapping("/login")
@@ -28,8 +33,21 @@ public class AuthController {
 
         // check if the admin exists and the password matches
         if (existingAdmin != null && existingAdmin.getPassword().equals(admin.getPassword())) {
-            // return successfull message if password matches
-            return ResponseEntity.ok("Login successful");
+        	// generate JWT token
+            String token = jwtUtil.generateToken(admin);
+
+            
+            // set the token as an HTTP-only cookie
+            ResponseCookie cookie = ResponseCookie.from("jwt", token)
+                .httpOnly(true)    // Prevent access via JavaScript
+                .secure(true)      // Ensure cookie is sent over HTTPS (optional in dev)
+                .path("/")         // cookie available for the entire application
+                .maxAge(60 * 60 * 2)   // expire in 2 hour
+                .build();
+
+            return ResponseEntity.ok()
+                .header("Set-Cookie", cookie.toString())
+                .body("Login successful");
         } else {
             // admin not found or incorrect password
             return ResponseEntity.status(401).body("Invalid credentials.");
